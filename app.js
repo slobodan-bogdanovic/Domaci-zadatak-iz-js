4f07a41f2de1179a532aa5c4647391e88c5358d2
slobodan-bogdanovic <slobobogdanovic1989@gmail.com>
2024-02-07 16:53:39 +0100

    dodao domaci

--- app.js
+++ app.js
@@ -7,32 +7,62 @@
 // objekat ces napraviti iznad svega
 // i return ide na kraj
 
-const osoba = {
-	ime: "Slobodan",
-	prezime: "Bogdanovic",
-	godine: 35,
-	srecniBrojevi: [3, 6, 10, 12, 13],
-};
+// const osoba = {
+// 	ime: "Slobodan",
+// 	prezime: "Bogdanovic",
+// 	godine: 35,
+// 	srecniBrojevi: [3, 6, 10, 12, 13],
+// };
 
-function ispisiOsobu(osoba) {
-	console.log(
-		`Cao, ja se zovem ${osoba.ime} prezivam se ${osoba.prezime} i imam ${osoba.godine} godina.`
-	);
+// function ispisiOsobu(korisnik) {
+// 	console.log(
+// 		`Cao, ja se zovem ${korisnik.ime} prezivam se ${korisnik.prezime} i imam ${korisnik.godine} godina.`
+// 	);
+// }
+
+// function zbirBrojeva(broj1, broj2, broj3) {
+// 	let zbir = broj1 + broj2 + broj3;
+// 	console.log(`Zbir ${broj1} ${broj2} i ${broj3} je jednak ${zbir}.`);
+// 	return zbir;
+// }
+
+// function listaSrecnihBrojeva() {
+// 	console.log(osoba.srecniBrojevi);
+// 	return osoba.srecniBrojevi.join(`-`);
+// }
+
+// ispisiOsobu(osoba);
+// const rezultatZbira = zbirBrojeva(
+// 	osoba.srecniBrojevi[2],
+// 	osoba.srecniBrojevi[3],
+// 	osoba.srecniBrojevi[0]
+// );
+// console.log(`Rezultat zbira je: ${rezultatZbira}`);
+// console.log(`Lista srecnih brojeva: ${listaSrecnihBrojeva()}`);
+
+// zadak domaci. Napraviti funkciju koja vraca poruke da li je broj paran i nije, pri tome gledaj da bude poruka i ako je nula
+//Kako mozemo da ispitamo da li je broj paran ili ne? Radili smo % ostatak? Znaci ako se podeli sa broj%2 a ostatak je 0 to znaci da je broj paran. U slucaju da ostatak nije nula onda je broj neparan. Imaj u vidu da stavis i uslov ako je broj nula...
+
+function brojParNepar(broj) {
+	if (broj === 0) {
+		return `broj je 0`;
+	} else if (broj % 2 === 0) {
+		return `broj je paran`;
+	} else {
+		return `broj je neparan`;
+	}
 }
+let Rezultat = brojParNepar(7);
+console.log(Rezultat);
 
-function zbirBrojeva(broj1, broj2, broj3) {
-	let zbir = broj1 + broj2 + broj3;
-	console.log(`Zbir ${broj1} ${broj2} i ${broj3} je jednak ${zbir}.`);
-	return zbir;
+//napravit funkciju koja ce da prima 2 argumenta, prvi argument ce biti da li je korisnik polozio tipovanje (znace bice boolean vrednost true ili false) a druga vrednost ce biti broj godina. funkcija ce da ispituje da li je korisnik adekvatan kandidat za polaganje voznje. Ako je korisnip polozio tipovanje i ima vise ili jednako 18 godina onda mozemo console log korisnik moze da polaze ispit. Ako korisnik nije polozio tipovanje ili nema dovolno godina mozemo da napisemo poruku 'korisnik nije ispunio uslove za polaganje'
+//bonus!!!ako slobo zavrsi sve ovo i bude besan, moze dodati koji je to razlog sto ne moze da polaze voznju
+
+function daLiKorisnikImaUslovZaPolaganje(polozioTipovanje, brojGodina) {
+	if (polozioTipovanje === true && brojGodina >= 18) {
+		console.log(`Korisnik moze da polaze ispit`);
+	} else {
+		console.log(`Korisnik nije ispunio uslove za polaganje`);
+	}
 }
-
-function listaSrecnihBrojeva() {
-	console.log(osoba.srecniBrojevi);
-	return osoba.srecniBrojevi;
-}
-
-ispisiOsobu(osoba);
-const rezultatZbira = zbirBrojeva(10, 30, 60);
-console.log(`Rezultat zbira je: ${rezultatZbira}`);
-const listaSretnihBrojeva = listaSrecnihBrojeva();
-console.log(`Lista sretnih brojeva: ${listaSretnihBrojeva}`);
+daLiKorisnikImaUslovZaPolaganje(true, 27);
